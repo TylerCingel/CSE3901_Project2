8e00d5a3d34e36f2468afd7e4d304df9f8200d94
Tyler Cingel <tccingel@gmail.com>
2021-02-07 14:39:30 -0500

    Tyler GameOfSet.rb push 2, card class

--- GameOfSet.rb
+++ GameOfSet.rb
@@ -1,3 +1,25 @@
 # Rediculous 6
 # CSE 3901
 # Project 2
+
+# Numbers: 1, 2, 3
+# Colors: Green, Blue, Red
+# Shapes: Diamond, Tilte, Oval
+# Opacity: Transparent, Translucent, Opaque
+
+class Card
+	attr_accessor :number, :color, :shape, :opacity
+	def initialize(number, color, shape, opacity)
+		@number = number
+		@color = color
+		@shape = shape
+		@opacity = opacity
+	end
+	def compare(card)
+		if (@number!=card.number && @color!=card.color && @shape!=card.shape && @opacity!=card.opacity)
+			puts "Unique"
+		else
+			puts "Not Unique"
+		end
+	end
+end
